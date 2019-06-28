8f5dc9ffcaa5d7ea9cf3f7e9bd8835ea88fe3032
rostIvan <bbwgd77@gmail.com>
2019-06-29 01:02:30 +0300

    Make snapshot fabfile.py, remove unnecessary $APP_PORT in docker-compose.yml

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,14 @@
+import json
 from os import environ as env
 
 from fabric import Connection
 from fabric import task
+
+REPO_NAME = 'schedule-DB'
+REPO_URL = f'https://github.com/thestd/{REPO_NAME}'
+
+APPS_ROOT = '~/application'
+PROJECT_ROOT = f'{APPS_ROOT}/{REPO_NAME}'
 
 
 @task
@@ -12,10 +19,42 @@ def staging(ctx):
 
 
 @task
-def deploy(ctx):
+def deploy(ctx, webhook_data):
+    data = json.loads(webhook_data)
+    if should_to_deploy(data):
+        print('Run deploy process...')
+        run_deploy(ctx, data)
+    else:
+        print('Skip deploy process...')
+
+
+def should_to_deploy(data):
+    action = data['action']
+    prerelease = data['release']['prerelease']
+    draft = data['release']['draft']
+    return (action == 'published'
+            and not prerelease
+            and not draft)
+
+
+def run_deploy(ctx, data):
     with remote_connection(ctx) as c:
-        with c.cd('~/'):
-            c.run('ls ../')
+        prepare(c)
+        with c.cd(PROJECT_ROOT):
+            tag = data['release']['tag_name']
+            deploy_process(c, tag)
+
+
+def prepare(c):
+    c.run(f'mkdir -p {APPS_ROOT}')
+    with c.cd(APPS_ROOT):
+        c.run(f'if [ ! -d {PROJECT_ROOT} ] ; then git clone {REPO_URL}; fi')
+
+
+def deploy_process(c, tag):
+    c.run('git fetch --all --tags --prune --prune-tags --progress')
+    c.run(f'git checkout -f tags/{tag} -B release/{tag}')
+    c.run(f'inv local-deploy')
 
 
 def remote_connection(ctx):
